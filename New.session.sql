7367f5648abcff67267530acb9ccb094c538ea52
AvirupRay <avirupraytun@gmail.com>
2024-09-27 22:13:53 +0530

    new table created

--- New.session.sql
+++ New.session.sql
@@ -20,4 +20,33 @@ SELECT * FROM users ;
 use blog;
 
 --@block
-CREATE TABLE 
+CREATE TABLE blogDet(
+    title varchar(255) not null,
+    details TEXT not null,
+    img varchar(255)
+);
+
+--@block
+drop TABLE blogdet
+
+--@block
+ALTER TABLE blogdet ADD email varchar(255);
+
+--@block
+ALTER TABLE blogdet add primary key(id);
+
+--@block
+alter table blogdet add foreign key (email) references users(email)
+
+--@block
+insert into blogdet(email,title,details,img)
+values
+    ('abc@gamil.com','Impact of AI','lorem ipsum','test image')
+;
+
+--@block
+ALTER TABLE blogdet
+MODIFY COLUMN id INT AUTO_INCREMENT;
+
+--@block //showing all data
+SELECT * FROM blogdet;
