18de6ce8b1d9fd65d37ff210d518b74d1b176f9a
AvirupRay <avirupraytun@gmail.com>
2024-09-27 22:14:21 +0530

    blogItem created

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 
-const BlogItem = () => {
+const BlogItem = (prop) => {
   return (
-    <div>
-      
+    <div className=' flex flex-col items-center justify-center w-1/4 border border-white mx-auto mt-5 shadow-[-4px_4px_0px_#fff] p-2 hover:shadow-[-6px_6px_0px_#fff] cursor-pointer'>
+      <img src={prop.pic} alt="" />
+      <h1 className=' text-2xl font-semibold font-mono'>{prop.title}</h1>
+      <p>{prop.desc}</p>
     </div>
   )
 }
