f8f371cd39ee436990ce3a68649989a262947895
AvirupRay <avirupraytun@gmail.com>
2024-09-27 22:15:47 +0530

    button changed

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Header = () => {
+
+  const clicked=async()=>{
+    console.log("clicked")
+  }
+
   return (
     <div className=' flex items-center justify-between px-10 p-5 '>
         <div className=' font-bold font-mono text-2xl select-none'>BlogO</div>
-        <button className='border-2 border-white p-2 shadow-[-6px_6px_0px_#fff] active:shadow-[-3px_3px_0px_#fff] font-semibold'>Get Started</button>
+        <button onClick={clicked} className='border-2 border-white p-2 shadow-[-6px_6px_0px_#fff] active:shadow-[-3px_3px_0px_#fff] font-semibold'>Add Blog</button>
     </div>
   )
 }
